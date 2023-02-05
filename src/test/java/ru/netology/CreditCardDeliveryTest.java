e9eaee226d0a0a34d92e5384bdffe46d08a966cb
sanyaminkin <sanyaminkin@gmail.com>
2023-02-06 02:34:59 +0300

    CreditCardDeliveryTest modified

--- src/test/java/ru/netology/CreditCardDeliveryTest.java
+++ src/test/java/ru/netology/CreditCardDeliveryTest.java
@@ -1,17 +1,24 @@
 package ru.netology;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.text.DateFormat;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditCardDeliveryTest {
 
@@ -22,32 +29,15 @@ public class CreditCardDeliveryTest {
 
     @Test
     void shouldMakeOrderTest() {
-        $("[data-test-id='city']").setValue("Санкт-Петербург");
-        $("[data-test-id='name']").setValue("Сергей Иванов-Петров");
-        $("[data-test-id='phone']").setValue("+71234567890");
+        $("[data-test-id='city']").$("[placeholder='Город']").setValue("Санкт-Петербург");
+        LocalDate localDate = LocalDate.now();
+        LocalDate newDate = localDate.plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = newDate.format(formatter);
+        $("[data-test-id='date']").$("[placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id= 'name']").$("[name ='name']").setValue("Николай Римский-Корсаков");
+        $("[data-test-id='phone']").$("[name='phone']").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
-        Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date newDate = instance.getTime();
-        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-        String dateText = dateFormat.format(instance);
-        $("[data-test-id='date']").setValue(dateText);
-        $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
-    }
-
-    @Test
-    void shouldMakeOrderTest2() {
-        $("[placeholder='Город']").setValue("Санкт-Петербург");
-        $("[name='name']").setValue("Сергей Иванов-Петров");
-        $("[name='phone']").setValue("+71234567890");
-        $("[data-test-id='agreement']").click();
-        Calendar instance = Calendar.getInstance();
-        instance.add(Calendar.DAY_OF_MONTH, 3);
-        Date newDate = instance.getTime();
-        DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
-        String dateText = dateFormat.format(instance);
-        $("[placeholder='Дата встречи']").setValue("dateText");
         $$("button").find(exactText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
     }
