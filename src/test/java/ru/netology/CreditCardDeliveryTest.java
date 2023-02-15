fd8449f791655572543337c75003b2de9072763b
sanyaminkin <sanyaminkin@gmail.com>
2023-02-15 20:47:55 +0300

    CreditCardDeliveryTest.java modified 2

--- src/test/java/ru/netology/CreditCardDeliveryTest.java
+++ src/test/java/ru/netology/CreditCardDeliveryTest.java
@@ -3,6 +3,7 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.SetValueOptions;
 import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,13 +25,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreditCardDeliveryTest {
 
-    public String generateDate(long addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
-    }
-
     @BeforeEach
     void setUp() {
         open("http://localhost:9999");
+    }
+
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     @Test
@@ -41,6 +42,8 @@ public class CreditCardDeliveryTest {
         //DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         //String dateText = newDate.format(formatter);
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+
+        String planningDate = new String();
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id= 'name']").$("[name ='name']").setValue("Николай Римский-Корсаков");
         $("[data-test-id='phone']").$("[name='phone']").setValue("+71234567890");
